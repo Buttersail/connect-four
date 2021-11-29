e196b5f6a428168f03880863cd535cd48beb12f2
Buttersail <h.m.f.m.92@gmail.com>
2021-11-29 12:36:35 +0100

    Scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # connect-four
+
+- Place a piece in the board
+  - Piece "falls" to the most available bottom on the board
+- Rinse and repeat until someone connects four
+- Expand with AI later on
---
+++ app.js
@@ -0,0 +1,7 @@
+// let currentPlayer = 'red'
+let gameActive = true
+
+// Seems to be only the first cell that reacts to this, figure out why
+document.querySelector('.cell').onclick = function () {
+  console.log('test')
+}
